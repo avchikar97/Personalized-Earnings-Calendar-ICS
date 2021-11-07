b1169655add7ea90bd93ad5c3673f77cedb879e2
avchikar97 <avchikar97@gmail.com>
2021-11-07 14:26:46 -0800

    Replace earnings calendar API with yahoo finance datascraper package

--- generate_ics.py
+++ generate_ics.py
@@ -1,7 +1,6 @@
-import investpy # earnings calendar API for us to filter through
+from yahoo_earnings_calendar import YahooEarningsCalendar  # earnings calendar API for us to filter through
 import ics # for creating the ICS file
 import datetime
-import sys
 from os import path
 import argparse
 
@@ -13,7 +12,8 @@ output_file = args.output_file
 input_filename = args.input_file
 
 def create_ics(output_filename, input_filename=None):
-    c = ics.Calendar(creator="Earnings Calendar Invites")
+    output_calendar = ics.Calendar(creator="Earnings Calendar Invites")
+    yec = YahooEarningsCalendar()
     added_stocks = []
     if(input_filename != None):
         file_ptr = open(file=input_filename, mode="r")
@@ -21,7 +21,6 @@ def create_ics(output_filename, input_filename=None):
         ### Stock selection
         if(input_filename != None):
             stock_name = file_ptr.readline().strip()
-            stock_country = "United States"
             if(len(stock_name) == 0):
                 print(f"Finished selecting {len(added_stocks)} stocks, outputting ICS.")
                 file_ptr.close()
@@ -31,38 +30,38 @@ def create_ics(output_filename, input_filename=None):
             if(stock_name == 'done'):
                 print(f"Finished selecting {len(added_stocks)} stocks, outputting ICS.")
                 break
-            stock_country = str(input("Stock country (default \'United States\'): ") or "United States")
 
         ### Getting the earnings date information and adding it to the calendar
-        if (stock_name, stock_country) not in added_stocks:
+        if (stock_name) not in added_stocks:
+            next_earnings_unix = 0;
             try:
                 ### Getting the information
-                info = investpy.stocks.get_stock_information(stock=stock_name, country=stock_country, as_json=True)
+                next_earnings_unix = int(yec.get_next_earnings_date(stock_name))
             except Exception as e:
                 print(e)
             else:
                 ### Extracting earnings date and reformatting for usage
-                earnings_date = info["Next Earnings Date"]
-                ics_earnings_date = datetime.datetime.strptime(earnings_date, '%d/%m/%Y').strftime('%Y-%m-%d')
-                human_earnings_date = datetime.datetime.strptime(earnings_date, '%d/%m/%Y').strftime('%m/%d/%Y')
+                earnings_date = datetime.datetime.utcfromtimestamp(next_earnings_unix).strftime('%Y-%m-%d %H:%M:%S')
+                ics_earnings_date = datetime.datetime.strptime(earnings_date, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+                human_earnings_date = datetime.datetime.strptime(earnings_date, '%Y-%m-%d %H:%M:%S').strftime('%m/%d/%Y')
                 
                 ### Deciding if it should be added to the calendar
-                add_to_calendar = str(input(f"Earnings date for {stock_name} ({stock_country}) found ({human_earnings_date}). Add to calendar (default Y)? [Y/n]: ") or "Y")
+                add_to_calendar = str(input(f"Earnings date for {stock_name} found ({human_earnings_date}). Add to calendar (default Y)? [Y/n]: ") or "Y")
                 if(add_to_calendar.lower() == "y"):
                     e = ics.Event(name=f"{stock_name} earnings", begin=ics_earnings_date)
                     e.make_all_day()
-                    c.events.add(e)
-                    added_stocks.append((stock_name, stock_country))
+                    output_calendar.events.add(e)
+                    added_stocks.append(stock_name)
                     print("Added to calendar.")
                 else:
                     print("Not added.")
         else:
-            print(f"{stock_name} ({stock_country}) has already been added to the calendar")
+            print(f"{stock_name} has already been added to the calendar")
         print() # makes the print a little easier on the eyes
 
     if(len(added_stocks) != 0):
         with open(output_filename, mode="w") as output_file:
-            output_file.writelines(c)
+            output_file.writelines(output_calendar)
             print(f"{output_filename} created with {len(added_stocks)} earnings dates.")
     else:
         print("No stocks were added to the calendar so no earnings calendar was created.")
